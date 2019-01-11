2ce550164fdc3a9ec28c50f0d8f9ed7242ab1f9d
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 19:19:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,21 +5,19 @@ class String
 
   def sentence?
     self.end_with?(".")
-   return true
+   #return true
   end
 
   def question?
     self.end_with?("?")
-  return 
+  #return false
   end
 
   def exclamation?
     self.end_with?("!")
-  return true
   end
 
   def count_sentences
     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
-    return false
   end
 end
